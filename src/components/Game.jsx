b6ae6bf8d9b9b6150c063dd678f8b449401265b7
SevRostomyan <41015978+SevRostomyan@users.noreply.github.com>
2023-07-31 05:21:29 +0200

    Jag har tagit bort ikonerna som visas efter varje drag. Har även fixat hur vinnaren ska visas samt aktiverat en enkel restart funktion som bara tar oss till startsidan. Har även stylat

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,15 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import { useLocation } from 'react-router-dom';
 import '../assets/StylesGame1.css';
+import { useNavigate } from "react-router-dom";
 
 function getChoiceImageURL(choice) {
+  if (!choice) {
+    return null; // or return a default image URL if you prefer
+  }
+
   return `../images/${choice.toLowerCase()}.png`;
 }
 
 function Game() {
   const location = useLocation();
-  const [userChoice, setUserChoice] = useState('rock')
-  const [user2Choice, setUser2Choice] = useState('rock')
+  const [userChoice, setUserChoice] = useState(null)
+  const [user2Choice, setUser2Choice] = useState(null)
   const [userPoints, setUserPoints] = useState(0)
   const [user2Points, setUser2Points] = useState(0)
   const [turnResult, setTurnResult] = useState(null)
@@ -24,6 +29,9 @@ function Game() {
   const [user2Score, setUser2Score] =useState(0)
   const [result_p, setResult_p] =useState('')
   const [intervalId, setIntervalId] = useState(null);
+  const [movesMade, setMovesMade] = useState(false);
+  const [userMove, setUserMove] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Starta intervallet när komponenten monteras
@@ -60,65 +68,87 @@ function Game() {
   }
 
   function fetchGameInfo() {
-    if (!gameInfo) return
-    fetch(`http://localhost:7979/api/games/${gameInfo?.gameId}`,{
+    
+    if (!gameInfo) return; // Om ingen spelinfo, returnera
+    fetch(`http://localhost:7979/api/games/${gameInfo?.gameId}`, {
       method: "post",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        // playerId: token
         playerId: gameInfo?.playerOne?.playerId
-        
       })
     })
     .then(response => response.json())
     .then(data => {
-      // console.log(gameInfo);
-      console.log(data, "gameData")
-        // gameData = data
-      // update game score:
-      setGameData(data)
+
+      setGameData(data); // Uppdatera speldata
+
+      // Uppdatera etiketter för spelare
       if (gameInfo?.playerOne?.playerName == data?.playerOne) {
-    //         user1Label.innerText = gameInfo.playerOne.playerName
-    //         user2Label.innerText = gameData.playerTwo
-    setUser1Label(gameInfo?.playerOne?.playerName === data?.playerOne ? gameInfo?.playerOne?.playerName : data?.playerOne)
-    setUser2Label(gameInfo?.playerOne?.playerName === data?.playerOne ? data?.playerTwo : gameInfo?.playerOne?.playerName)
+        setUser1Label(gameInfo?.playerOne?.playerName);
+        setUser2Label(data?.playerTwo);
+      } else {
+        setUser1Label(data?.playerOne);
+        setUser2Label(gameInfo?.playerOne?.playerName);
+      }
 
+      // Uppdatera spelarens poäng
+      setUserScore(data?.playerOneWins);
+      setUser2Score(data?.playerTwoWins);
 
-          } else {
-    //         user1Label.innerText = gameData.playerOne
-    //         user2Label.innerText = gameInfo.playerOne.playerName
-    setUser1Label(data?.playerOne)
-    setUser2Label(gameInfo?.playerOne?.playerName)
-    }
-    setUserScore(data?.playerOneWins)
-    setUser2Score(data?.playerTwoWins)
+     
+      // If the game is over, announce the winner. Otherwise, display the moves.
+    if (data?.status === 'PLAYER_ONE_IS_THE_WINNER' || data?.status === 'PLAYER_TWO_IS_THE_WINNER') {
+      setGameOver(true);
+      setResult_p(data?.status === 'PLAYER_ONE_IS_THE_WINNER' ? `${data?.playerOne.toUpperCase()} ÄR VINNAREN!!!` : `${data?.playerTwo.toUpperCase()} ÄR VINNAREN!!!`);
+    } else {
+      if (gameInfo?.playerOne?.playerName == data?.playerOne) {
+        setResult_p(data?.playerTwoMove ? `Du ${data?.playerOneMove}, ${data?.playerTwo} ${data?.playerTwoMove}` : 'Väntar på drag...');
+      } else {
+        setResult_p(data?.playerOneMove ? `Du ${data?.playerTwoMove}, ${data?.playerOne} ${data?.playerOneMove}` : 'Väntar på drag...');
+      }
+    
+      }
 
-            if (gameInfo?.playerOne?.playerName == data?.playerOne) {
-              // result_p.innerText = gameData.playerOneMove
-              // result_p.innerText = `You ${gameData.playerOneMove}, ${gameData.playerTwo} ${gameData.playerTwoMove}`
-              setResult_p(`You ${data?.playerOneMove}, ${data?.playerTwo} ${data?.playerTwoMove}`)
+      // Om båda spelarna har gjort sina drag
+      if (data?.playerOneMove && data?.playerTwoMove) {
+        setUserChoice(gameInfo?.playerOne?.playerName == data?.playerOne ? data?.playerOneMove.toLowerCase() : data?.playerTwoMove.toLowerCase());
+        setUser2Choice(gameInfo?.playerOne?.playerName == data?.playerOne ? data?.playerTwoMove.toLowerCase() : data?.playerOneMove.toLowerCase());
+        
+        setMovesMade(true); // Om båda spelarna har gjort sina drag, sätt movesMade till true
+        setUserMove(null); // Återställ userMove efter att båda spelarna har gjort sina drag
 
-            } else {
-              // result_p.innerText = gameData.playerTwoMove
-              // result_p.innerText = `You ${gameData.playerTwoMove}, ${gameData.playerOne} ${gameData.playerOneMove}`
-              setResult_p(`You ${data?.playerTwoMove}, ${data?.playerOne} ${data?.playerOneMove}`)
-            };
+        // Visa rätt resultatmeddelande
+        if (gameInfo?.playerOne?.playerName == data?.playerOne) {
+          setResult_p(`Du ${data?.playerOneMove}, ${data?.playerTwo} ${data?.playerTwoMove}`);
+        } else {
+          setResult_p(`Du ${data?.playerTwoMove}, ${data?.playerOne} ${data?.playerOneMove}`);
+        }
+      } else {
+        setMovesMade(false); // Om någon spelare inte har gjort ett drag ännu, sätt movesMade till false
+      }
 
-            if (data?.playerOneMove && data?.playerTwoMove) {
-              setUserChoice(data?.playerOneMove.toLowerCase());
-              setUser2Choice(data?.playerTwoMove.toLowerCase());
-            }
+      // Om spelet är över
+      if (data?.status === 'PLAYER_ONE_IS_THE_WINNER' || data?.status === 'PLAYER_TWO_IS_THE_WINNER') {
+        setGameOver(true);
+        setResult_p(data?.status === 'PLAYER_ONE_WIN' ? `${data?.playerOne} is the winner!!!` : `${data?.playerTwo} is the winner!!!`);
+      }
+
     })
     .catch(error => {
-      console.log(error, "createGame");
+      console.log(error, "createGame"); // Visa felmeddelande vid fel
     });
 }
-// setInterval(fetchGameInfo, 1000);
+
 
 function makeMove(move) {
-  setUserChoice(move); //user's choice state variable
+  console.log('User made move:', move);
+
+  setGameOver(false);
+
+  //setUserChoice(move); //user's choice state variable
+  setMovesMade(false); //We set movesMade state variable to false here
   if (!gameInfo) return
   fetch(`http://localhost:7979/api/games/move`,{
     method: "post",
@@ -136,8 +166,12 @@ function makeMove(move) {
   })
   .then(response => response.json())
   .then(data => {
-      setGameData(data)
+    setGameData(data)
     console.log(gameInfo, 'Moved')
+    setUserMove(move); // Set the user move after making the move
+  
+
+
   })
   .catch(error => {
     console.log(error, "createGame");
@@ -191,43 +225,41 @@ function makeMove(move) {
         <h1 className='score-user1'>{user1Label} : {userScore}</h1>
         <h1 className='score-user2'>{user2Label}: {user2Score}</h1>
       </div>
-      {/* <div className='choice'>
-        <div className='choice-user'>
-          <img className='user-hand' src={`../images/${gameData?.playerOneMove}.png`} alt="" />
-        </div>
-        <div className='choice-computer'>
-          <img className='computer-hand' src={`../images/${gameData?.playerTwoMove}.png`} alt="" />
-        </div>
-      </div> */}
+    
+
+{/*Har satt ikonens state variabeln default värde till null och nedan sett till att det inte crashar om det returneras null istället för ikonvärdet*/}
 
 <div className='choice'>
-        <div className='choice-user1'>
-          <img className='user1-hand' src={getChoiceImageURL(userChoice)} alt="" />
-        </div>
-        <div className='choice-user2'>
-          <img className='user2-hand' src={getChoiceImageURL(user2Choice)} alt="" />
-        </div>
-      </div>
-      <div className='button-div'>
-        {choices.map((choice, index) => (
-          <button className="button" key={choice} onClick={() => makeMove(choice)}>
-            <img src={getChoiceImageURL(choice)} alt={choice} />
-          </button>
-        ))}
-      </div>
+  <div className='choice-user1'>
+    {userChoice && <img className='user1-hand' src={getChoiceImageURL(userChoice)} alt="" />}
+  </div>
+  <div className='choice-user2'>
+    {user2Choice && <img className='user2-hand' src={getChoiceImageURL(user2Choice)} alt="" />}
+  </div>
+</div>
+{!gameOver && <div className='button-div'>
+  {choices.map((choice, index) => (
+    <button className="button" key={choice} onClick={() => makeMove(choice)}>
+      <img src={getChoiceImageURL(choice)} alt={choice} />
+    </button>
+  ))}
+</div> }
 
 
-      <div className='result'>
-        {/* <h1>Turn Result: {turnResult}</h1>
-        <h1>Final Result: {result}</h1> */}
-        <h1>{result_p}</h1>
 
-      </div>
-      <div className='button-div'>
-        {gameOver && 
-          <button className='button' onClick={() => reset()}>Restart Game</button>
-        }
-      </div>
+
+<div className='finalResult'>
+  {gameOver && <h1>{result_p}</h1>}
+  {!gameOver && movesMade && <h1>{result_p}</h1>}
+  {!gameOver && !movesMade && userMove && <h1>You {userMove}</h1>}
+  
+</div>
+
+
+<div className='button-div'>
+  {gameOver && <button className='button' onClick={() => navigate('/')}>Restart Game</button>}
+</div>
+
 
       {/* <div className='play-online-btn'>
         <button>
